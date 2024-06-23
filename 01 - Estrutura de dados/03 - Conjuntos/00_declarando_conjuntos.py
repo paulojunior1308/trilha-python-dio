2ebdbc96f944fb5e522d3297a8e088a9a3e4618e
paulojunior1308 <pauloesjr2@gmail.com>
2024-06-23 15:20:44 -0300

    Criando e acessando o conjunto (set)

---
+++ 01 - Estrutura de dados/03 - Conjuntos/00_declarando_conjuntos.py
@@ -0,0 +1,5 @@
+print(set([1, 2, 3, 1, 3, 4])) # {1, 2, 3, 4}
+
+print(set("abacaxi")) # {'c', 'a', 'i', 'b', 'x'}
+
+print(set(("palio", "gol", "celta", "palio"))) # {'celta', 'gol', 'palio'}
---
+++ 01 - Estrutura de dados/03 - Conjuntos/01_acessando_dados.py
@@ -0,0 +1,7 @@
+numeros = {1, 2, 3, 2}
+
+numeros = list(numeros)
+
+print (numeros[0])
+
+# Não conseguimos acessar um item do conjunto set, somente quando convertemos para uma lista
