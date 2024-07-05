e3b2649c3d8eb633a950af86f1879b0e6e87941a
paulojunior1308 <pauloesjr2@gmail.com>
2024-07-05 11:56:51 -0300

    Python DB API

---
+++ 08 - Banco de dados/02_injecao_sql.py
@@ -0,0 +1,14 @@
+import sqlite3
+from pathlib import Path
+
+ROOT_PATH = Path(__file__).parent
+
+
+conexao = sqlite3.connect(ROOT_PATH / "meu_banco.db")
+cursor = conexao.cursor()
+cursor.row_factory = sqlite3.Row
+
+id_cliente = input("Informe o id do cliente: ")
+cursor.execute("SELECT * FROM clientes WHERE id=?", (id_cliente,))
+cliente = cursor.fetchone()
+print(dict(cliente))
